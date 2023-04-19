5838727c671adf06e80141e560aa1d474995ac93
רועי נחמן ברנד <brand_ro@mac.org.il>
2023-04-19 18:29:34 +0300

    webapi

--- WebApp_UnderTheHood/Startup.cs
+++ WebApp_UnderTheHood/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp_UnderTheHood.Authorization;
 
 namespace WebApp_UnderTheHood
 {
@@ -23,6 +25,31 @@ namespace WebApp_UnderTheHood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
+            {
+                options.Cookie.Name = "MyCookieAuth";
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.ExpireTimeSpan = TimeSpan.FromSeconds(30);
+            });
+
+            services.AddAuthorization(options => {
+
+                options.AddPolicy("AdminOnly",
+                    policy => policy.RequireClaim("Admin"));
+
+                options.AddPolicy("MustBelongToHRDepartment",
+                    policy => policy.RequireClaim("Department", "HR"));
+
+                options.AddPolicy("HRManagerOnly", policy => policy
+                    .RequireClaim("Department", "HR")
+                    .RequireClaim("Manager")
+                    .Requirements.Add(new HRManagerProbationRequirement(3)));
+
+            });
+
+            services.AddSingleton<IAuthorizationHandler, HRManagerProbationRequirementHandler>();
+
             services.AddRazorPages();
         }
 
@@ -45,6 +72,7 @@ namespace WebApp_UnderTheHood
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
